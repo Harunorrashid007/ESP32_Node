a609f876c4b2b36804479870a4c4c7f2677b4a1a
Craig Peacock <craigp@beyondlogic.org>
2021-04-22 20:29:15 +0930

    Added support for INA228

--- main/main.c
+++ main/main.c
@@ -7,6 +7,7 @@
 #include "i2c.h"
 #include "bme280.h"
 #include "ina226.h"
+#include "ina228.h"
 #include "adxl345.h"
 
 void app_main(void)
@@ -17,9 +18,20 @@ void app_main(void)
 	//bme280_init(I2C_CONTROLLER_0);
 
 	i2c_init(I2C_CONTROLLER_1, 22, 23);
-	adxl345_init(I2C_CONTROLLER_1);
+	//adxl345_init(I2C_CONTROLLER_1);
+	ina228_init(I2C_CONTROLLER_1);
 
 	while (true) {
+
+		printf("Voltage       = %.03f V\r\n", ina228_voltage(I2C_CONTROLLER_1));
+		printf("Die Temp      = %.01f degC\r\n", ina228_dietemp(I2C_CONTROLLER_1));
+		printf("Shunt Voltage = %.03f mV\r\n", ina228_shuntvoltage(I2C_CONTROLLER_1));
+		printf("Current       = %.03f A\r\n", ina228_current(I2C_CONTROLLER_1));
+		printf("Power         = %.03f W\r\n", ina228_power(I2C_CONTROLLER_1));
+		printf("Energy        = %.05f Wh\r\n", ina228_energy(I2C_CONTROLLER_1) / 3600);
+		printf("Charge        = %.05f Ah\r\n", ina228_charge(I2C_CONTROLLER_1) / 3600);
+		printf("\r\n");
+
 		//printf("Temperature = %.02f degC, ", bme280_temp(I2C_CONTROLLER_0));
 		//printf("Pressure = %.02f hPa, ",bme280_pressure(I2C_CONTROLLER_0));
 
@@ -27,11 +39,11 @@ void app_main(void)
 		//printf("Current = %.02f A, ", ina226_current(I2C_CONTROLLER_1));
 		//printf("Power = %.02f W\r\n", ina226_power(I2C_CONTROLLER_1));
 
-		printf("X Axis = %3d, ", adxl345_read_x(I2C_CONTROLLER_1));
-		printf("Y Axis = %3d, ", adxl345_read_y(I2C_CONTROLLER_1));
-		printf("Z Axis = %3d\r\n", adxl345_read_z(I2C_CONTROLLER_1));
+		//printf("X Axis = %3d, ", adxl345_read_x(I2C_CONTROLLER_1));
+		//printf("Y Axis = %3d, ", adxl345_read_y(I2C_CONTROLLER_1));
+		//printf("Z Axis = %3d\r\n", adxl345_read_z(I2C_CONTROLLER_1));
 
-		vTaskDelay(100 / portTICK_PERIOD_MS);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
 }
 
